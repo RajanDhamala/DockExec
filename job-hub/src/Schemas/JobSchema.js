b15e41f6e57caad3fd557db069319dcd6e2ca990
RajanDhamala <Rajandhamala0123@gmail.com>
2025-12-17 21:32:28 +0545

    schema design for mongodb

--- job-hub/src/Schemas/JobSchema.js
+++ job-hub/src/Schemas/JobSchema.js
@@ -1,13 +1,36 @@
+import { string } from "joi";
 import mongoose from "mongoose";
-const JobSchema = new mongoose.Schema({
-problemId: { type: mongoose.Schema.Types.ObjectId, ref: "Problem", required: true },
-userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-language: { type: String, required: true },
-Usercode: { type: String, required: true },
-GeneratedCode: { type: String, required: true },
-status: { type: String, enum: ["Pending", "Running", "Completed", "Failed"], default: "Pending" },
-output:{ type: String },
-},{timestamps:true});
+const RunCodeSchema = new mongoose.Schema({
+  _id:{
+    type:string,
+    unique:true
+  },
+  problemid: {
+    type: mongoose.schema.types.objectid,
+    ref: "Problem", required: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", required: true
+  },
+  language: {
+    type: String, required: true
+  },
+  Usercode: {
+    type: String, required: true
+  },
+  GeneratedCode: {
+    type: String, required: true
+  },
+  status: {
+    type: String
+    , enum: ["Success", "Failed"],
+    default: "Success"
+  },
+  output: {
+    type: String
+  },
+}, { timestamps: true });
 
-const Job= mongoose.model("Job", JobSchema)
+const Job = mongoose.model("Job",RunCodeSchema )
 export default Job;
--- job-hub/src/Schemas/RunTestSchema.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose"
-
-const RunResultSchema=new mongoose.Schema({
-   jobId:{type:mongoose.Schema.Types.ObjectId,ref:"Job",required:true}, 
-   
-})
