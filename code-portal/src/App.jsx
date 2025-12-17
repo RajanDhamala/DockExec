18d844ee7bcc70d2dbf9954144dbd7601d1052a8
RajanDhamala <Rajandhamala0123@gmail.com>
2025-12-17 21:27:14 +0545

    packages update & routes inited

--- code-portal/src/App.jsx
+++ code-portal/src/App.jsx
@@ -1,15 +1,19 @@
 
 import { Suspense } from "react";
 import "./index.css";
-import {LazyLandingPage,LazyRegisterPage,LazyLoginPage,LazyTestPage,} from "./LazyLoading/LazyLoading";
+import {LazyLandingPage,LazyRegisterPage,LazyLoginPage,LazyTestPage,LazyWritePage, LazyLeetCode} from "./LazyLoading/LazyLoading";
 import {BrowserRouter as Router,Routes,Route,} from "react-router-dom";
 import {QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./Utils/QueryConfig.jsx";
 import Loader from "./LazyLoading/Loader.jsx";
 import { Toaster } from "react-hot-toast";
-
+import useSocketStore from "./ZustandStore/SocketStore";
+import { useEffect } from "react";
 function App() {
-
+   const { initSocket, socket, isConnected } = useSocketStore();
+  useEffect(() => {
+    initSocket(); // initialize socket on mount
+  }, []);
 
    return (
     <QueryClientProvider client={ queryClient}>
@@ -18,10 +22,10 @@ function App() {
        <Suspense fallback={<Loader />}>
        <Routes>
             <Route path="/" element={<LazyLandingPage />} />
-            <Route path="/login" element={<LazyLoginPage />} />
-            <Route path="/register" element={<LazyRegisterPage />} />
-            <Route path="/test" element={<LazyTestPage />} />
-
+            <Route path="/auth/:mode" element={<LazyLoginPage />} />
+            <Route path="/code" element={<LazyWritePage />} />
+            <Route path="/leet" element={<LazyLeetCode />} />
+    
             <Route path="*" element={<div className="p-10 text-center text-red-500 font-bold">404 | Page Not Found</div>} />
           </Routes>
         </Suspense>
